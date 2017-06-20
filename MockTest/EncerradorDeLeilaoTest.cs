de96b6218db15a97a20c37ae58d88c54e8b6eb78
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-06-20 16:55:45 -0400

    Adicionando tudo.

---
+++ MockTest/EncerradorDeLeilaoTest.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using mock.dominio;
+using mock.infra;
+using mock.servico;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MockTest
+{
+    [TestClass]
+    public class EncerradorDeLeilaoTest
+    {
+        private Leilao leilao;
+        private Leilao leilao2;
+        private Leilao leilao3;
+        private Mock<RepositorioDeLeiloes> dao;
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            leilao = new Leilao("TV 21 pol");
+            leilao.naData(new DateTime(2017, 06, 11));
+            leilao2 = new Leilao("Play 4");
+            leilao2.naData(new DateTime(2017, 06, 10));
+            leilao3 = new Leilao("XBox");
+            leilao3.naData(DateTime.Now);
+            dao = new Mock<RepositorioDeLeiloes>();
+        }
+        [TestMethod]
+        public void DeveEncerarLeiloesComMaisDeUmaSemana()
+        {
+            List<Leilao> retorno = new List<Leilao>() {
+                leilao,
+                leilao2
+            };
+
+            dao.Setup(m => m.correntes()).Returns(retorno);
+
+            var encerrador = new EncerradorDeLeilao(dao.Object);
+            encerrador.encerra();
+
+            Assert.AreEqual(2, encerrador.total);
+            Assert.IsTrue(leilao.encerrado);
+            Assert.IsTrue(leilao2.encerrado);
+        }
+        [TestMethod]
+        public void NaoDeveEncerrarLeilaoDeHoje()
+        {
+            List<Leilao> retorno = new List<Leilao>() {
+                leilao3,
+                leilao
+            };
+
+            dao.Setup(m => m.correntes()).Returns(retorno);
+
+            var encerrador = new EncerradorDeLeilao(dao.Object);
+            encerrador.encerra();
+
+            Assert.AreEqual(1, encerrador.total);
+            Assert.IsFalse(leilao3.encerrado);
+            Assert.IsTrue(leilao.encerrado);
+        }
+        [TestMethod]
+        public void NaoDeveFazerNadaCasoNaoHajaLeiloes()
+        {
+            dao.Setup(m => m.correntes()).Returns(new List<Leilao>());
+            var encerrador = new EncerradorDeLeilao(dao.Object);
+            encerrador.encerra();
+
+            Assert.AreEqual(0, encerrador.total);
+        }
+    }
+}
---
+++ mock/servico/EncerradorDeLeilao.cs
@@ -0,0 +1,56 @@
+﻿using mock.dominio;
+using mock.infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mock.servico
+{
+    public class EncerradorDeLeilao
+    {
+        public int total { get; private set; }
+        public RepositorioDeLeiloes Dao { get; private set; }
+
+        public EncerradorDeLeilao(RepositorioDeLeiloes dao)
+        {
+            total = 0;
+            Dao = dao;
+        }
+
+        public virtual void encerra()
+        {
+            List<Leilao> todosLeiloesCorrentes = Dao.correntes();
+
+            foreach (var l in todosLeiloesCorrentes)
+            {
+
+                if (comecouSemanaPassada(l))
+                {
+
+                    l.encerra();
+                    total++;
+                    Dao.atualiza(l);
+
+                }
+            }
+        }
+
+
+        private bool comecouSemanaPassada(Leilao leilao)
+        {
+
+            return diasEntre(leilao.Data, DateTime.Now) >= 7;
+
+        }
+
+        private int diasEntre(DateTime inicio, DateTime fim)
+        {
+            int dias = (int)(fim - inicio).TotalDays;
+
+            return dias;
+        }
+
+    }
+}
